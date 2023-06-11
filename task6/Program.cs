a45ac80ef50dde3de1bb289ce57eab5c77036441
Maksim-Stefanishin <stefanishin-91@mail.ru>
2023-06-11 14:50:18 +0300

    Task 6

---
+++ task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n%2==0)
+{
+    Console.Write($"{n} is even");
+}
+else if(n%2!=0)
+{
+    Console.Write($"{n} is odd");
+}
