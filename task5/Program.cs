ac28ee674ef8a6d76fe69a1e6a0d04ad7ff3b1b7
Maksim-Stefanishin <stefanishin-91@mail.ru>
2023-06-11 13:34:18 +0300

    Task 5

---
+++ task5/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Input number: ");
+int number =Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int count = 0;
+while(count<=2* number)
+{
+    Console.Write($"{-number + count++}, ");
+}
