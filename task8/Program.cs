a09dea4f7848373636e79b6014c389b30d8fbf36
Maksim-Stefanishin <stefanishin-91@mail.ru>
2023-06-12 00:05:43 +0300

    Task8

---
+++ task8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Input number: ");
+int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int count = 2;
+while (count <= n)
+{
+        Console.Write($"{(count++)} ");
+        count++;
+}    
