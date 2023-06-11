d13771c28306f6504f2662c84bb7733a45cd8a59
Maksim-Stefanishin <stefanishin-91@mail.ru>
2023-06-11 12:19:17 +0300

    Task 3

---
+++ task3/Program.cs
@@ -0,0 +1,39 @@
+﻿
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1)
+{
+    Console.Write("It's mondey");
+}
+else if (number == 2)
+{
+    Console.Write("It's tuesday");
+}
+else if (number == 3)
+{
+    Console.Write("It's wednesday");
+}
+else if (number == 4)
+{
+    Console.Write("It's Thrusday");
+}
+else if (number == 5)
+{
+    Console.Write("It's Friday");
+}
+else if (number == 6)
+{
+    Console.Write("It's Saturday");
+}
+else if (number == 7)
+{
+    Console.Write("It's Sanday");
+}
+else if (number > 7)
+{
+    Console.Write("Such day isn't exist");
+}
+else if (number == 0)
+{
+    Console.Write("Such day isn't exist");
+}
