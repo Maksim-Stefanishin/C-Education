8fe465e2cb63f8fa0379980e317533d70e4dcecf
Maksim-Stefanishin <stefanishin-91@mail.ru>
2023-06-11 14:34:58 +0300

    Task 4

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Input number 1: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number 2: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number 3: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+if (n1 > n2 && n1 > n3)
+{
+Console.Write($"Max number {n1}");
+}
+else if (n2 > n1 && n2 > n3)
+{
+Console.Write($"Max number {n2}");
+}
+else if (n3 > n1 && n3 > n2)
+{
+Console.Write($"Max number {n3}");
+}
