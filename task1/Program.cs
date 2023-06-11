20797e3d16801d1793773eb7dcef4cf8d72d8b7c
Maksim-Stefanishin <stefanishin-91@mail.ru>
2023-06-11 12:05:22 +0300

    Task 1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input number 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+    Console.Write($"{number1} is quater {number2}");
+}
+else
+{
+    Console.Write($"{number1} isn't quater {number2}");
+}
