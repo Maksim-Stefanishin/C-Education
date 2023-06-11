2b484a35387428b82b4587562dcb774d88907b3f
Maksim-Stefanishin <stefanishin-91@mail.ru>
2023-06-11 12:29:00 +0300

    Task 3 Re:1

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,39 +1,12 @@
 ﻿
 Console.Write("Input number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number == 1)
-{
-    Console.Write("It's mondey");
-}
-else if (number == 2)
-{
-    Console.Write("It's tuesday");
-}
-else if (number == 3)
-{
-    Console.Write("It's wednesday");
-}
-else if (number == 4)
-{
-    Console.Write("It's Thrusday");
-}
-else if (number == 5)
-{
-    Console.Write("It's Friday");
-}
-else if (number == 6)
-{
-    Console.Write("It's Saturday");
-}
-else if (number == 7)
-{
-    Console.Write("It's Sanday");
-}
-else if (number > 7)
-{
-    Console.Write("Such day isn't exist");
-}
-else if (number == 0)
-{
-    Console.Write("Such day isn't exist");
-}
+if (number == 1) Console.Write("It's mondey");
+if (number == 2) Console.Write("It's tuesday");
+if (number == 3) Console.Write("It's wednesday");
+if (number == 4) Console.Write("It's Thrusday");
+if (number == 5) Console.Write("It's Friday");
+if (number == 6) Console.Write("It's Saturday");
+if (number == 7) Console.Write("It's Sanday");
+if (number > 7) Console.Write("Such day isn't exist");
+if (number == 0) Console.Write("Such day isn't exist");
