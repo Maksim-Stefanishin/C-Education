b0188234258e8881f972264c1cfe4dc5f26c4b57
Maksim-Stefanishin <stefanishin-91@mail.ru>
2023-06-12 00:18:16 +0300

    add description

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.Write("Input number 1: ");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.Write("Input number 1: ");
 int n1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input number 2: ");
 int n2 = Convert.ToInt32(Console.ReadLine());
--- task8/Program.cs
+++ task8/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Write("Input number: ");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.Write("Input number: ");
 int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 int count = 2;
 while (count <= n)
