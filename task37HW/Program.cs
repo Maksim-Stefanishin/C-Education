25ace78ee4387230d0ddb8becb6aef0dd3aa1603
Maksim-Stefanishin <stefanishin-91@mail.ru>
2023-06-25 02:02:48 +0300

    HW

---
+++ task37HW/Program.cs
@@ -0,0 +1,30 @@
+﻿//  Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] FillArrayWithRandomNumber(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(100, 999);
+    }
+    return arr;
+}
+
+System.Console.Write("Введите кол-во элементов массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(string.Join("; ", array));
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] %2 != 0)
+    {
+       count= count+array[i];
+    }
+}
+System.Console.WriteLine(count);
---
+++ task38HW/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+double[] FillArrayWithRandomNumber(int size)
+{
+   double[] array = new double[size];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0,5) + rand.NextDouble();
+            }
+ 
+            return array;
+}
+
+System.Console.Write("Введите кол-во элементов массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+double[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(string.Join("; ", array));
+double min = array[0];
+double max = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+     if ( array[i] < min)
+     {
+        min = array[i] ;
+     }
+     else if (array[i] > max)
+     {
+        max = array[i] ;
+     }
+}
+double result = max - min;
+
+System.Console.WriteLine($"Разница между макисмальным и минимальным значением {result} ");
+
